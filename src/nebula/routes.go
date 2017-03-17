de145f229c724dab42c47ca2c2bc4d0c20d91ee6
timmonfette1 <monfette.timothy@gmail.com>
2017-03-17 15:45:06 -0400

    Initial Commit

---
+++ src/nebula/routes.go
@@ -0,0 +1,75 @@
+package main
+
+import "net/http"
+
+type Route struct {
+  Name        string
+  Method      string
+  Pattern     string
+  HandlerFunc http.HandlerFunc
+}
+
+type Routes []Route
+
+var routes = Routes{
+  Route{
+    "Index",
+    "GET",
+    "/",
+    Index,
+  },
+  Route{
+    "AddObject",
+    "POST",
+    "/additem",
+    AddItem,
+  },
+  Route{
+    "AddCat",
+    "POST",
+    "/addcat",
+    AddCat,
+  },
+  Route{
+    "GetItems",
+    "GET",
+    "/getitems/{collName}",
+    GetItems,
+  },
+  Route{
+    "GetCats",
+    "GET",
+    "/getcats",
+    GetCats,
+  },
+  Route{
+    "GetDetails",
+    "GET",
+    "/getdetails/{collName}/{id}",
+    GetDetails,
+  },
+  Route{
+    "GetUserInfo",
+    "GET",
+    "/getuserinfo/{username}",
+    GetUserInfo,
+  },
+  Route{
+    "GetUserItems",
+    "GET",
+    "/getuseritems/{username}",
+    GetUserItems,
+  },
+  Route{
+    "CheckUsername",
+    "GET",
+    "/checkusername/{username}",
+    CheckUsername,
+  },
+  Route{
+    "AddUser",
+    "POST",
+    "/adduser",
+    AddUser,
+  },
+}
